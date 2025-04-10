37a8f54b07ce3dc91b0cf91b88bf153c8312f5cc
vromanyu <vromanyu@netcompany.com>
2025-04-10 16:19:32 +0300

    lower logging level and remove debug config

--- src/main/java/com/vromanyu/spring_security_jwt/config/SecurityConfiguration.java
+++ src/main/java/com/vromanyu/spring_security_jwt/config/SecurityConfiguration.java
@@ -20,7 +20,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import java.util.Collections;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 public class SecurityConfiguration {
 
  @Bean
